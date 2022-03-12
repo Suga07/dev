cd1fa2c6351d584d25ad1eb19d30618f3334b8a2
thanvi07 <sugashini@codincity.com>
2022-03-12 19:26:40 +0000

    fourth.one commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('expect miracles:02!')
+  response.send('expect miracles:03!')
 })
 
 app.listen(app.get('port'), function() {
