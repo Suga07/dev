f6d513b3967dec9be1cd568dbe311c0f6708ec1d
root <root@ip-172-31-93-251.ec2.internal>
2022-03-12 19:23:51 +0000

    fourth commit

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('expect miracles:01!')
+  response.send('expect miracles:02!')
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+}
+)
